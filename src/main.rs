8dd5598c943a5a2aa2cdd8144b3d61eeebcd34e2
Pablo Almeida <estebanpablo89@email.com>
2025-02-03 18:47:46 -0500

    divide in modules

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,8 @@
-#[derive(Debug)]
-enum Media {
-    Book { title: String, author: String },
-    Movie { title: String, director: String },
-    Audiobook { title: String },
-    Podcast(u32),
-    Placeholder,
-}
+mod content;
 
-impl Media {
-    fn description(&self) -> String {
-        // if let Media::Book { title, author } = self {
-        //     format!("Book: {} {}", title, author)
-        // } else if let Media::Movie { title, director } = self {
-        //     format!("Movie: {} {}", title, director)
-        // } else if let Media::Audiobook { title } = self {
-        //     format!("Audiobook: {}", title)
-        // } else {
-        //     String::from("Media description")
-        // }
+use content::media::Media;
+use content::catalog::Catalog;
 
-        match self {
-            Media::Book { title, author } => {
-                format!("Book: {} {}", title, author)
-            }
-            Media::Movie { title, director } => {
-                format!("Movie: {} {}", title, director)
-            }
-            Media::Audiobook { title } => {
-                format!("Audiobook: {}", title)
-            }
-            Media::Podcast(id) => {
-                format!("Podcast: {}", id)
-            }
-            Media::Placeholder => {
-                format!("Placeholder")
-            }
-        }
-    }
-}
-
-#[derive(Debug)]
-struct Catalog {
-    items: Vec<Media>,
-}
-
-impl Catalog {
-    fn new() -> Self {
-        Catalog { items: vec![] }
-    }
-
-    fn add(&mut self, media: Media) {
-        self.items.push(media);
-    }
-
-    fn get_by_index(&self, index: usize) -> Option<&Media> {
-        if self.items.len() > index {
-            // Good! We have somethign to return
-            Some(&self.items[index])
-        } else {
-            // Bad! We don't have anything to return!!!
-            None
-        }
-    }
-}
 
 enum MightHaveAValue<'a> {
     ThereIsAValue(&'a Media),
